8528834df8d59ade585d059fca4e937bfc2b7da6
asuvarnaeworldes79 <amitsuvarna@eworldes.com>
2024-12-31 21:17:45 +0530

    fixed footer nav

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Link, Box, Typography } from "@mui/material";
+import { Toolbar, Link, Box, Typography } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { memo } from "react";
 import { FooterProps } from "../../types/IFooter";
@@ -10,18 +10,18 @@ const Footer = (props: FooterProps) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Box>
-        {footerLeftPanel && (
-          <div className="flex flex-grow flex-shrink-0">
-            {footerLeftPanel}
-          </div>
-        )}
-        {footerRightPanel && (
-          <div className="flex flex-grow flex-shrink-0 px-12 justify-end">
-            {footerRightPanel}
-          </div>
-        )}
-      </Box>
+      <Toolbar className="min-h-48 md:min-h-64 px-8 sm:px-12 py-0 flex items-center overflow-x-auto">
+          {footerLeftPanel && (
+            <div className="flex flex-grow flex-shrink-0">
+              {footerLeftPanel}
+            </div>
+          )}
+          {footerRightPanel && (
+            <div className="flex flex-grow flex-shrink-0 px-12 justify-end">
+              {footerRightPanel}
+            </div>
+          )}
+        </Toolbar>
       <Box
         sx={(theme) => ({
           padding: 2,
